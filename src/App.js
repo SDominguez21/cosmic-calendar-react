64fc672d5382534584ef85c2727c790c76a3d015
Monroe <mgetz10@gmail.com>
2019-08-03 14:24:41 -0400

    memory.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React from "react";
-import { Route, Switch, Link } from "react-router-dom";
-import axios from "axios";
-import Home from "./components/home/Home.js";
-import Navbar from "./components/navbar/Navbar.js";
-import Signup from "./components/signup/Signup.js";
-import Login from "./components/login/Login.js";
-import AllForcasts from "./components/all-forcasts/AllForcasts";
-import AllMoon from "./components/all-moon/AllMoon";
-import ForcastDetails from "./components/forcast-details/ForcastDetails";
+import React from 'react';
+import { Route, Switch, Link } from 'react-router-dom';
+import axios from 'axios';
+import Home from './components/home/Home.js';
+import Navbar from './components/navbar/Navbar.js';
+import Signup from './components/signup/Signup.js';
+import Login from './components/login/Login.js';
+import AllForcasts from './components/all-forcasts/AllForcasts';
+import AllMoon from './components/all-moon/AllMoon';
+import ForcastDetails from './components/forcast-details/ForcastDetails';
 // import AllCosmicEvents from "./components/all-comsic-events/AllaCosmicEvents";ll
-// import Moment from 'react-moment';
+import Moment from 'react-moment';
 // import AuthService from "./services/AuthService.js";
 
 class App extends React.Component {
@@ -29,44 +29,56 @@ class App extends React.Component {
     // this.service = new AuthService();
   }
 
-  fetchEvents = async () => {
-    console.log("for the love of god");
-    let weather = await axios.get(
-      `https://api.aerisapi.com/forecasts/miami,fl?filter=daynight&limit=60&format=json&client_id=ubVTEqqaNjJ2GI2wGFHqj&client_secret=DTmrLknIoE2Bk2HOBj6jHKJqcj0CBDK5KhyZTkoR`
-    );
-
-    let moon = await axios.get(
-      `https://api.aerisapi.com/sunmoon/moonphases?limit=31&miami,fl&from=08/02/2019&format=json&client_id=ubVTEqqaNjJ2GI2wGFHqj&client_secret=DTmrLknIoE2Bk2HOBj6jHKJqcj0CBDK5KhyZTkoR
-    `
-    );
-
-    let actualWeatherArray = weather.data.response[0].periods;
-    console.log(actualWeatherArray);
-
-    let actualmoon = moon.data.response[0];
-    console.table(moon.data.response[0])
-
-    this.setState({
-      listOfForcasts: weather.data,
-      visibleForcast: weather.data,
-      ready: true
-    });
-    this.setState({
-      listOfMoonEvents: moon.data,
-      visibleMoon: moon.data,
-      ready: true
-    });
+  today = async () => {
+    console.log('getting todays stuff');
+    await axios
+      .get('http://localhost:5000/today')
+      .then(response => {
+        console.log(response.data[1].timestamp);
+        return response.data[1].timestamp;
+      })
+      .catch(() => {
+        console.log('err');
+      });
   };
+  // fetchEvents = async () => {
+  //   console.log("for the love of god");
+  //   let weather = await axios.get(
+  //     `https://api.aerisapi.com/forecasts/miami,fl?filter=daynight&limit=60&format=json&client_id=ubVTEqqaNjJ2GI2wGFHqj&client_secret=DTmrLknIoE2Bk2HOBj6jHKJqcj0CBDK5KhyZTkoR`
+  //   );
+
+  //   let moon = await axios.get(
+  //     `https://api.aerisapi.com/sunmoon/moonphases?limit=31&miami,fl&from=08/02/2019&format=json&client_id=ubVTEqqaNjJ2GI2wGFHqj&client_secret=DTmrLknIoE2Bk2HOBj6jHKJqcj0CBDK5KhyZTkoR
+  //   `
+  //   );
+
+  //   let actualWeatherArray = weather.data.response[0].periods;
+  //   console.log(actualWeatherArray);
+
+  //   let actualmoon = moon.data.response[0];
+  //   console.table(moon.data.response[0])
+
+  //   this.setState({
+  //     listOfForcasts: weather.data,
+  //     visibleForcast: weather.data,
+  //     ready: true
+  //   });
+  //   this.setState({
+  //     listOfMoonEvents: moon.data,
+  //     visibleMoon: moon.data,
+  //     ready: true
+  //   });
+  // };
 
   // Sign up/log in
   getCurrentlyLoggedInUser = () => {
-    console.log("FETCHING USER!!!!");
+    console.log('FETCHING USER!!!!');
     axios
-      .get("http://localhost:5000/api/auth/getcurrentuser", {
+      .get('http://localhost:5000/api/auth/getcurrentuser', {
         withCredentials: true
       })
       .then(response => {
-        console.log("yay really fetching the user now");
+        console.log('yay really fetching the user now');
         let theUser = response.data;
         this.setState({ currentlyLoggedIn: theUser });
         return theUser;
@@ -79,24 +91,25 @@ class App extends React.Component {
   toggleForm = whichForm => {
     let theForm;
 
-    if (whichForm === "signup") {
-      theForm = "signupShowing";
+    if (whichForm === 'signup') {
+      theForm = 'signupShowing';
     } else {
-      theForm = "loginShowing";
+      theForm = 'loginShowing';
     }
 
     this.setState({ [theForm]: !this.state[theForm] });
   };
 
   componentDidMount() {
-    this.fetchEvents();
+    // this.fetchEvents();
+    this.today();
     this.getCurrentlyLoggedInUser();
   }
 
   logout = () => {
     axios
       .post(
-        "http://localhost:5000/api/auth/logout",
+        'http://localhost:5000/api/auth/logout',
         {},
         { withCredentials: true }
       )
@@ -112,7 +125,7 @@ class App extends React.Component {
   // if I want a search a bar this is where the function would go
 
   render() {
-    console.log("*************", this.state);
+    console.log('*************', this.state);
 
     return (
       <div>
@@ -185,6 +198,7 @@ class App extends React.Component {
             )}
           />
         </Switch>
+        <Moment unix>{this.today()}</Moment>
         yay! this is my app
       </div>
     );
